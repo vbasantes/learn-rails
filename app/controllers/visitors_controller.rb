da79a7bb3ac25164f37230e0cccf629d94d33d3c
Victor Basantes <victor_basantes@hotmail.com>
2021-12-02 08:32:50 -0500

    Use rails layout gem to create better setup for application

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,8 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
+    flash.now[:notice] = 'Welcome'
+    flash.now[:alert] = "My birthday is soon"
     render 'visitors/new'
   end
 
